3f5ece9dff07a9fa0bdb63adde4c74a092bf6bcc
danielklioc <danielklioc@gmail.com>
2023-01-30 22:48:41 +0200

    create file setup

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git clone https://github.com/tpm2-software/tpm2-tss
+git clone https://github.com/tpm2-software/tpm2-tools
+git clone https://github.com/tpm2-software/tpm2-abrmd
+git clone https://github.com/tpm2-software/tpm2-pkcs11
+git clone https://github.com/OpenSC/libp11.git
